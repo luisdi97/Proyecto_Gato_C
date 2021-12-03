263c41d469e3ba7c7f9e613bdaaf5f9334954477
Luis Diego Araya Campos <luisdiegoarayacampos@hotmail.com>
2021-12-03 10:56:03 -0600

    The relative PATH of the glade file is used. And the glade file is added

--- Tic_Tac_Toe.c
+++ Tic_Tac_Toe.c
@@ -59,7 +59,7 @@ int main(int argc, char* argv[]) {
 
     // Get GUI from file
     builder = gtk_builder_new_from_file(
-        "/home/luisdi/Desktop/Tic_Tac_Toe.glade");
+        "./Tic_Tac_Toe.glade");
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
