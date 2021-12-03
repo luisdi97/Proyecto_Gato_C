337cba8182d5baabfd6d3dac0fe86161d4f1f7f6
Luis Diego Araya Campos <luisdiegoarayacampos@hotmail.com>
2021-12-03 16:33:29 -0600

    Functional game. Buttons are disabled on winning event. Reset pending.

--- Tic_Tac_Toe.c
+++ Tic_Tac_Toe.c
@@ -4,8 +4,8 @@
 char check_rows(char (*game_state)[3]){
     int a;
     int b;
-    _Bool X_wins[] = {0, 0, 0};
-    _Bool O_wins[] = {0, 0, 0};
+    _Bool X_wins[3][3] = {{0, 0, 0},{0, 0, 0},{0, 0, 0}};
+    _Bool O_wins[3][3] = {{0, 0, 0},{0, 0, 0},{0, 0, 0}};
     char current_box;
 
     for (a = 0; a < 3; a++) {
@@ -13,18 +13,25 @@ char check_rows(char (*game_state)[3]){
             current_box = *(*(game_state + a) + b);
 
             if (current_box=='X') {
-                X_wins[b] = 1;
+                X_wins[a][b] = 1;
             } else if (current_box=='O') {
-                O_wins[b] = 1;
+                O_wins[a][b] = 1;
             }
         }
     }
 
     char output_char = ' ';
 
-    if (X_wins[0]&&X_wins[1]&&X_wins[2]) {
+    if (X_wins[0][0]&&X_wins[0][1]&&X_wins[0][2] ||
+        X_wins[1][0]&&X_wins[1][1]&&X_wins[1][2] ||
+        X_wins[2][0]&&X_wins[2][1]&&X_wins[2][2]) {
+
         output_char = 'X';
-    } else if (O_wins[0]&&O_wins[1]&&O_wins[2]) {
+
+    } else if (O_wins[0][0]&&O_wins[0][1]&&O_wins[0][2] ||
+               O_wins[1][0]&&O_wins[1][1]&&O_wins[1][2] ||
+               O_wins[2][0]&&O_wins[2][1]&&O_wins[2][2]) {
+
         output_char = 'O';
     }
 
@@ -34,8 +41,8 @@ char check_rows(char (*game_state)[3]){
 char check_columns(char (*game_state)[3]){
     int a;
     int b;
-    _Bool X_wins[] = {0, 0, 0};
-    _Bool O_wins[] = {0, 0, 0};
+    _Bool X_wins[3][3] = {{0, 0, 0},{0, 0, 0},{0, 0, 0}};
+    _Bool O_wins[3][3] = {{0, 0, 0},{0, 0, 0},{0, 0, 0}};
     char current_box;
 
     for (a = 0; a < 3; a++) {
@@ -43,18 +50,25 @@ char check_columns(char (*game_state)[3]){
             current_box = *(*(game_state + b) + a);
 
             if (current_box=='X') {
-                X_wins[a] = 1;
+                X_wins[b][a] = 1;
             } else if (current_box=='O') {
-                O_wins[a] = 1;
+                O_wins[b][a] = 1;
             }
         }
     }
 
     char output_char = ' ';
 
-    if (X_wins[0]&&X_wins[1]&&X_wins[2]) {
+    if (X_wins[0][0]&&X_wins[1][0]&&X_wins[2][0] ||
+        X_wins[0][1]&&X_wins[1][1]&&X_wins[2][1] ||
+        X_wins[0][2]&&X_wins[1][2]&&X_wins[2][2]) {
+
         output_char = 'X';
-    } else if (O_wins[0]&&O_wins[1]&&O_wins[2]) {
+
+    } else if (O_wins[0][0]&&O_wins[1][1]&&O_wins[2][2] ||
+               O_wins[0][1]&&O_wins[1][1]&&O_wins[2][1] ||
+               O_wins[0][2]&&O_wins[1][1]&&O_wins[2][2]) {
+
         output_char = 'O';
     }
 
@@ -157,45 +171,87 @@ void on_clicked_button(
   *(*(game_state + i) + j) = *current_player;
 
   char winning_player = check_three_in_line(game_state);
+  GtkWidget *((*buttons)[3]);
+  buttons = arguments->buttons;
+  GtkLabel *player_label = (GtkLabel*) arguments->player_label;
+
+
 
   if (winning_player != ' ') {
-      g_print("%c wins\n",winning_player);
+      // g_print("%c player wins\n",winning_player);
+
+      char state = *current_player;
+      char *str;
+
+      if (state == 'X') {
+        str = g_strdup_printf ("<span font=\"90\" color=\"red\">"
+                                     "<b>%s</b>"
+                                     "</span>",
+                                     "X");
+      } else if (state == 'O') {
+        str = g_strdup_printf ("<span font=\"90\" color=\"blue\">"
+                                     "<b>%s</b>"
+                                     "</span>",
+                                     "O");
+      }
+
+      GtkButton *button = (GtkButton*) *(*(buttons + i) + j);
+      GtkLabel *button_label = (GtkLabel*) gtk_bin_get_child(GTK_BIN(button));
+      gtk_label_set_markup (GTK_LABEL (button_label), str);
+      g_free (str); // remember to free the string allocated by g_strdup_printf()
+
+      // Allocates storage
+      char *winning_string = (char*)malloc(14 * sizeof(char));
+
+      sprintf(winning_string, "Jugador %c gana", winning_player);
+
+      gtk_label_set_text(player_label, winning_string);
+
+      free(winning_string);
+
+      int a;
+      int b;
+
+      for (a = 0; a < 3; a++) {
+          for (b = 0; b < 3; b++) {
+              GtkWidget *button_widget = *(*(buttons + a) + b);
+              gboolean sensitive = FALSE;
+              gtk_widget_set_sensitive(button_widget, sensitive);
+          }
+      }
+  }else{
+      char state = *current_player;
+      char *str;
+
+      if (state == 'X') {
+        str = g_strdup_printf ("<span font=\"90\" color=\"red\">"
+                                     "<b>%s</b>"
+                                     "</span>",
+                                     "X");
+
+        gtk_label_set_text(player_label, "Jugador O");
+
+        *current_player = 'O';
+
+      } else if (state == 'O') {
+        str = g_strdup_printf ("<span font=\"90\" color=\"blue\">"
+                                     "<b>%s</b>"
+                                     "</span>",
+                                     "O");
+
+        gtk_label_set_text(player_label, "Jugador X");
+
+        *current_player = 'X';
+
+      }
+
+        GtkButton *button = (GtkButton*) *(*(buttons + i) + j);
+        GtkLabel *button_label = (GtkLabel*) gtk_bin_get_child(GTK_BIN(button));
+        gtk_label_set_markup (GTK_LABEL (button_label), str);
+        g_free (str); // remember to free the string allocated by g_strdup_printf()
+        gboolean sensitive = FALSE;
+        gtk_widget_set_sensitive(widget, sensitive);
   }
-
-  char state = *current_player;
-  GtkLabel *player_label = (GtkLabel*) arguments->player_label;
-  char *str;
-
-  if (state == 'X') {
-    str = g_strdup_printf ("<span font=\"90\" color=\"red\">"
-                                 "<b>%s</b>"
-                                 "</span>",
-                                 "X");
-
-    gtk_label_set_text(player_label, "Jugador O");
-
-    *current_player = 'O';
-
-  } else if (state == 'O') {
-    str = g_strdup_printf ("<span font=\"90\" color=\"blue\">"
-                                 "<b>%s</b>"
-                                 "</span>",
-                                 "O");
-
-    gtk_label_set_text(player_label, "Jugador X");
-
-    *current_player = 'X';
-
-  }
-    GtkWidget *((*buttons)[3]);
-    buttons = arguments->buttons;
-
-    GtkButton *button = (GtkButton*) *(*(buttons + i) + j);
-    GtkLabel *button_label = (GtkLabel*) gtk_bin_get_child(GTK_BIN(button));
-    gtk_label_set_markup (GTK_LABEL (button_label), str);
-    g_free (str); // remember to free the string allocated by g_strdup_printf()
-    gboolean sensitive = FALSE;
-    gtk_widget_set_sensitive(widget, sensitive);
 }
 
 int main(int argc, char* argv[]) {
